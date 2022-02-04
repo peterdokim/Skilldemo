f24100d665bf626ba85e5b7e2acf67a400624e66
stopdatkimmy <peterjhms1922@gmail.com>
2022-02-04 15:14:28 -0800

    lib added + failing test

--- javafile.java
+++ javafile.java
@@ -1,5 +1,9 @@
 public class javafile{
 
+    public static int subtract(int n, int m){
+        return n-m;
+    }
+
 
 
 }
---
+++ javafiletest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class javafiletest {
+
+    @Test
+
+    public void subtract(){
+
+        assertEquals(1,javafile.subtract(100, 20));
+    }
+    
+}
